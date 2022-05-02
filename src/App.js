fa2c9870290d3f44ff635887e21d76d29117632c
mstars_lab3_14 <mstars_lab3_14@mstars-lab3-14s-iMac.local>
2022-05-02 12:51:16 +0800

    initialize

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Par from "./Par.js";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello my first React</h1>
+      <Par />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Par.js
@@ -0,0 +1,40 @@
+function Para() {
+  return (
+    <p className="text">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure, tenetur
+      eveniet in suscipit odio voluptatum temporibus cupiditate! Aut
+      exercitationem qui tempora, dolore aspernatur fugit blanditiis iusto esse
+      pariatur vitae unde aliquam cupiditate illum quisquam culpa itaque, cumque
+      saepe iste laudantium suscipit incidunt quae totam harum eaque! Eos sequi,
+      quos adipisci molestiae recusandae ut! Voluptate, reprehenderit. Velit
+      dicta cum nobis tempore libero nemo a nulla! Labore, culpa eligendi fugit
+      eaque laudantium quo cumque, blanditiis dolorem perferendis rerum ducimus,
+      libero vel laboriosam expedita eos a dolor. Nostrum ut dolore est eos
+      cumque similique vitae dolorum consequatur rem qui, rerum esse quas
+      explicabo laudantium veritatis, consectetur nulla. Eos, assumenda? Ipsum
+      laborum esse enim odio eveniet! Ipsa minus earum facere mollitia ea
+      laboriosam non quidem illum? Quae sint cum, molestiae eos blanditiis
+      voluptatibus consequatur similique ullam corrupti dolore, quisquam iste
+      minus est rerum repudiandae? Sunt at fugit facilis dolore? Nemo, obcaecati
+      fuga id pariatur aut perferendis magni quo officiis harum asperiores alias
+      laborum adipisci ipsum deleniti, autem eum repellendus perspiciatis esse
+      magnam mollitia doloremque, molestiae fugit! Perspiciatis, atque eum
+      tenetur voluptates eos dignissimos maxime aut pariatur odio omnis
+      perferendis hic voluptatibus animi aspernatur nisi dolores nesciunt
+      assumenda libero praesentium. Aut, eveniet! Iste voluptate totam velit!
+      Dolorum libero qui, illo sed quos inventore molestiae, omnis architecto
+      culpa fugiat ea voluptates accusantium illum commodi suscipit minus, amet
+      quam quidem nulla error itaque saepe. Voluptas porro sequi aliquam enim
+      veritatis neque commodi nihil dolor quia architecto atque minima
+      laudantium recusandae, voluptates corrupti illo. Hic dicta, pariatur ipsum
+      officiis possimus eaque alias, dignissimos fuga culpa quis nihil? Alias
+      laudantium velit consequuntur sit magni incidunt dolorem expedita esse
+      obcaecati. Suscipit tempora maxime vel nesciunt ab consequatur ipsa odit
+      voluptates incidunt, magni soluta voluptatum aut nisi odio quis quod
+      dolorem blanditiis, fuga cum quaerat modi distinctio mollitia!
+      Necessitatibus, tempore dignissimos.
+    </p>
+  );
+}
+
+export default Para;
